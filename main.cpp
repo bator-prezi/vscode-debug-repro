2f244fc78a4eee6a1e73a17abd8d4c1c53979640
bator-prezi <bator.taller@prezi.com>
2019-12-09 20:56:30 +0100

    VSCode C++ debug bug repro

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main() {
+	int i = GetValue();
+	i += 5;
+	
+	return i;
+}
---
+++ other_folder/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+int GetValue() {
+	int val = 8;
+	for (int i = 0; i < 10; ++i) {
+		val += i;
+	}
+	return 5;
+}
